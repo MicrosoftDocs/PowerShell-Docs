e76a87e1f60b9aa74b0c62a7710f9a4d000e2cde
joeyaiello <jaiello@microsoft.com>
2016-10-21 20:44:38 -0700

    fix appveyor across versions

--- appveyor.ps1
+++ appveyor.ps1
@@ -2,6 +2,7 @@ $ReferenceDocset = Join-Path $PSScriptRoot 'reference'
 
 Get-ChildItem $ReferenceDocset -Directory `
 | ForEach-Object -Process {
+    $Version = $_
     $VersionFolder = $_.FullName
     Get-ChildItem $VersionFolder -Directory `
     | ForEach-Object -Process {
@@ -9,8 +10,8 @@ Get-ChildItem $ReferenceDocset -Directory `
         $ModulePath = Join-Path $VersionFolder $_
       
         $LandingPage = Join-Path $ModulePath "$ModuleName.md"
-        $MamlOutputFolder = Join-Path "$PSScriptRoot\out_maml" $ModuleName
-        $CabOutputFolder = Join-Path "$PSScriptRoot\out_cab" $ModuleName
+        $MamlOutputFolder = Join-Path "$PSScriptRoot\out_maml" "$Version\$ModuleName"
+        $CabOutputFolder = Join-Path "$PSScriptRoot\out_cab" "$Version\$ModuleName"
         
 
         New-ExternalHelp -Path $ModulePath -OutputPath $MamlOutputFolder
