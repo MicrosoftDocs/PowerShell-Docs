61440d75dcc819881c07c2e2963662bcf518617d
Sergei Vorobev <sevoroby@microsoft.com>
2016-11-17 17:43:33 -0800

    Make AppVeyor build fail on platyPS errors

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,20 +1,33 @@
-$ReferenceDocset = Join-Path $PSScriptRoot 'reference'
+param(
+    [switch]$SkipCabs
+)
 
-Get-ChildItem $ReferenceDocset -Directory `
-| ForEach-Object -Process {
+$ReferenceDocset = Join-Path $PSScriptRoot 'reference'
+$allErrors = @()
+
+Get-ChildItem $ReferenceDocset -Directory | ForEach-Object -Process {
     $Version = $_
     $VersionFolder = $_.FullName
-    Get-ChildItem $VersionFolder -Directory `
-    | ForEach-Object -Process {
+    Get-ChildItem $VersionFolder -Directory | ForEach-Object -Process {
         $ModuleName = $_        
         $ModulePath = Join-Path $VersionFolder $_
       
         $LandingPage = Join-Path $ModulePath "$ModuleName.md"
         $MamlOutputFolder = Join-Path "$PSScriptRoot\out_maml" "$Version\$ModuleName"
         $CabOutputFolder = Join-Path "$PSScriptRoot\out_cab" "$Version\$ModuleName"
-        
 
-        New-ExternalHelp -Path $ModulePath -OutputPath $MamlOutputFolder
-        New-ExternalHelpCab -CabFilesFolder $MamlOutputFolder -LandingPagePath $LandingPage -OutputFolder $CabOutputFolder 
+        try {
+            New-ExternalHelp -Path $ModulePath -OutputPath $MamlOutputFolder -Force
+            if (-not $SkipCabs) {
+                New-ExternalHelpCab -CabFilesFolder $MamlOutputFolder -LandingPagePath $LandingPage -OutputFolder $CabOutputFolder
+            }
+        } catch {
+            $allErrors += $_
+        }
     }
+}
+
+if ($allErrors) {
+    $allErrors
+    throw "There are errors during platyPS run!`nPlease fix your markdown to comply with the schema: https://github.com/PowerShell/platyPS/blob/master/platyPS.schema.md"
 }
