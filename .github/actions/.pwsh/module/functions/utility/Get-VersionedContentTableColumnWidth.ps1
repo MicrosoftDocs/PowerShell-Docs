375fdf010d31d85aaafd83a4d8abff5bee9ce565
Michael Lombardi <mlombardi@microsoft.com>
2022-06-13 17:09:57 -0500

    (AB-1951698) Add Changed Content Version Reporting
    When managing documentation with multiple versions of the same file, it
    is necessary to check Pull Requests modifying versioned content across
    all of the versions that file exists for. It is not always obvious from
    GitHub's view for files changed in a Pull Request whether and how a file
    was modified across versions.
    
    Prior to this change, the only way for the maintainers to inspect this
    information was to carefully read every change in the Pull Request. It
    required a large amount of cognitive load as a reviewer needed to make a
    mental note of every change to versioned content, find the other
    versions of that file (if any) the Pull Request modified, and verify
    whether all needed changes were made.
    
    This change helps to address the problem by introducing a new GitHub
    Action for reporting on how a Pull Request modified versioned content.
    
    The new `reporting/versioned-content` action inspects the files changed
    in a Pull Request to determine which files are versioned content and
    then writes a report by folder enumerating which files the Pull Request
    changed and how those changes impact each version for the file.
    
    The action is called in this repository by the new `versioned-content`
    workflow, which runs on Pull Requests to the `main` branch which modify
    versioned content in the `reference` folder.
    
    Like the other actions, `reporting/versioned-content` can be called from
    other repositories.
    
    This change also added new utility functions to the GHA module:
    
    - `Add-VersionedContentTable` writes a markdown table summarizing a
      changeset.
    - `Get-VersionedContentChangeStatus` analyzes a Pull Request's changes
      to return information about the changes to versioned content.
    - `Get-VersionedContentTableColumnWidth` returns the calculated widths
      for columns in the report's tables to ensure the pipes in the tables
      line up for reading when interactively debugging.
    
    All three functions are exported by the module.
    
    Finally, the action utilizes the updated `Get-VersionedContentReport`
    script, which is now appropriately documented.
    
    - Resolves AB#1951698

---
+++ .github/actions/.pwsh/module/functions/utility/Get-VersionedContentTableColumnWidth.ps1
@@ -0,0 +1,175 @@
+<#
+.SYNOPSIS
+
+    Calculates the maximum width of columns for a versioned content table.
+
+.DESCRIPTION
+
+    This cmdlet calculates the maximum width of columns for a versioned content table for a given
+    changeset, returning the minimum widths for these columns:
+
+    - **RelativePath:** Used for the **VersionRelativePath** value for each changeset item.
+    - **Version:** Used for the **ChangeType** value for every version of each changeset item.
+
+    By default, it returns the widths for each defined column in the order listed above.
+
+.PARAMETER ChangeSet
+
+    Specify one or more changesets to calculate the column widths for a versioned content table
+    enumerating these changes.
+
+.PARAMETER DefaultWidth
+
+    Specify an integer as the default width for all calculated column widths. If this value is not
+    specified, each column has its own default width:
+
+    - **RelativePath:** `30` characters wide.
+    - **Version:** `12` characters wide.
+
+.PARAMETER RelativePath
+
+    Specify this switch to return only the calculated width for the **RelativePath** column.
+
+.PARAMETER Version
+
+    Specify this switch to return only the calculated width for the **Version** columns.
+
+.EXAMPLE
+
+    ```powershell
+    $ChangeSet = @(
+        [PSCustomObject]@{
+            VersionRelativePath = 'foo.md'
+            Versions = @(
+                @{ Version = '1.1'; ChangeType =  'N/A' }
+                @{ Version = '1.2'; ChangeType =  'unchanged' }
+                @{ Version = '1.3'; ChangeType =  'modified' }
+            )
+        }
+        [PSCustomObject]@{
+            VersionRelativePath = 'bar/baz.md'
+            Versions = @(
+                @{ Version = '1.1' ; ChangeType = 'added' }
+                @{ Version = '1.2' ; ChangeType = 'added' }
+                @{ Version = '1.3' ; ChangeType = 'added' }
+            )
+        }
+    )
+    $RelativePathWidth, $VersionWidth = Get-VersionedContentTableColumnWidth -ChangeSet $ChangeSet
+    "Relative Path Column Width: $RelativePathWidth"
+    "Version Column Width:       $VersionWidth"
+    ```
+
+    ```output
+    Relative Path Column Width: 30
+    Version Column Width:       12
+    ```
+
+    The first command constructs an arbitrary changeset.
+
+    The second command uses this cmdlet to determine the column widths for a table describing that
+    changeset, assigning the output to the `$RelativePathWidth` and `$VersionWidth` variables. Because
+    the **DefaultWidth** parameter was not specified, the cmdlet uses default values for each column
+    (`30` for **RelativePath** and `12` for **Version**).
+
+    The last two commands display the results. Because none of the data in the changeset has a
+    longer path or version than the defaults for the columns, the output values are the same as the
+    default values for each column.
+
+.EXAMPLE
+
+    ```powershell
+    $ChangeSet = @(
+        [PSCustomObject]@{
+            VersionRelativePath = 'foo.md'
+            Versions = @(
+                @{ Version = '1.1'; ChangeType =  'N/A' }
+                @{ Version = '1.2'; ChangeType =  'unchanged' }
+                @{ Version = '1.3'; ChangeType =  'modified' }
+            )
+        }
+        [PSCustomObject]@{
+            VersionRelativePath = 'bar/baz.md'
+            Versions = @(
+                @{ Version = '1.1' ; ChangeType = 'added' }
+                @{ Version = '1.2' ; ChangeType = 'added' }
+                @{ Version = '1.3' ; ChangeType = 'added' }
+            )
+        }
+    )
+    Get-VersionedContentTableColumnWidth -ChangeSet $ChangeSet -RelativePath -DefaultWidth 5
+    ```
+
+    ```output
+    14
+    ```
+    
+    The first command constructs an arbitrary changeset.
+
+    The second command uses this cmdlet to determine the width of the **RelativePath** column for a
+    table describing that changeset. It limits the calculations to only the **RelativePath** column
+    by using the switch parameter of the same name. It specifies the minimum width of the column to
+    `5`.
+
+    The widest **VersionRelativePath** property in the changeset is `bar/baz.md`, which is `10`
+    characters wide. The calculation adds an additional four characters to this width to account for
+    the wrapping backticks and spaces in the table entry. The result value is therefore `14`.
+
+#>
+
+function Get-VersionedContentTableColumnWidth {
+    [CmdletBinding(DefaultParameterSetName='AllColumns')]
+    [OutputType([Int[]])]
+    [OutputType([int], ParameterSetName=('RelativePathOnly','VersionOnly'))]
+    param(
+        [Parameter(Mandatory, ParameterSetName='RelativePathOnly')]
+        [Parameter(Mandatory, ParameterSetName='VersionOnly')]
+        [Parameter(Mandatory, ParameterSetName='AllColumns')]
+        [ValidateNotNullOrEmpty()]
+        $ChangeSet,
+
+        [Parameter(ParameterSetName='RelativePathOnly')]
+        [Parameter(ParameterSetName='VersionOnly')]
+        [Parameter(ParameterSetName='AllColumns')]
+        [int]$DefaultWidth,
+
+        [Parameter(ParameterSetName='RelativePathOnly')]
+        [switch]$RelativePath,
+
+        [Parameter(ParameterSetName='VersionOnly')]
+        [switch]$Version
+    )
+
+    process {
+        #region    RelativePath
+        if ($RelativePath -or !$Version) {
+            # Set the default width for RelativePath unless specified by user
+            $RelativePathWidth = ($DefaultWidth -lt 1) ? 30 : $DefaultWidth
+            # Loop over changesets to find longest path & set as width
+            $ChangeSet.VersionRelativePath | ForEach-Object -Process {
+                $PathEntryLength = Split-Path -Path $_ -Leaf | Select-Object -ExpandProperty Length
+                $PathEntryLength += 4
+                if ($PathEntryLength -gt $RelativePathWidth) {
+                    $RelativePathWidth = $PathEntryLength
+                }
+            }
+            # Write the calculated RelativePath column width to output
+            $RelativePathWidth
+        }
+        #endregion RelativePath
+        if ($Version -or !$RelativePath) {
+            # Set the default width for Version unless specified by user
+            $VersionWidth = ($DefaultWidth -lt 1) ? 12 : $DefaultWidth
+            # Find unique versions and loop over them to find longest version & set as width
+            $VersionList = $ChangeSet.Versions.Version | Select-Object -Unique
+            $VersionList | ForEach-Object -Process {
+                $VersionLength = $_.Length + 2
+                if ($VersionLength -gt $VersionWidth) {
+                    $VersionWidth = $VersionLength
+                }
+            }
+            # Write the calculated Version column width to output
+            $VersionWidth
+        }
+    }
+}
