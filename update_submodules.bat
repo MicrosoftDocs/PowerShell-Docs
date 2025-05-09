7f331c358d6b0d34ad1b44c3733d92c42bb7f5d5
mccoy stevens <webmaster@samsungknox.co>
2025-05-09 03:36:49 +0100

    Add files via upload

---
+++ update_submodules.bat
@@ -0,0 +1,5 @@
+#git submodule update --recursive
+#git submodule foreach git pull origin master
+#git pull --recurse-submodules
+#git submodule foreach git fetch --progress --all
+git submodule foreach "git pull origin master || git pull origin main || git pull origin atari || :"
